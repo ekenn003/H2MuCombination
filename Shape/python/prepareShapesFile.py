30661d7ef39a3a9c5610a0a5b0f44c8b838fb335
E Kennedy <ekennedy@cern.ch>
2017-06-28 03:45:21 +0200

    plit to tools

--- Shape/python/prepareShapesFile.py
+++ Shape/python/prepareShapesFile.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 import os, sys
 from ROOT import *
+from tools.tools import *
 
 main_data_dir  = '{0}/src/H2MuCombination/data'.format(os.environ['CMSSW_BASE'])
 shape_data_dir = '{0}/src/H2MuCombination/Shape/data'.format(os.environ['CMSSW_BASE'])
 input_file_head = 'ana_2Mu_'
 input_file_tail = '.root'
 degree = 4
-blind_low, blind_high = 120., 130.
-range_low, range_high = 105., 200.
-sig_fit_low, sig_fit_high = 115., 135.
+
+ranges = {
+    'blind_low'  : 120.,
+    'blind_high' : 130.,
+    'range_low'  : 105.,
+    'range_high' : 200.,
+    'sig_fit_low'  : 115.,
+    'sig_fit_high' : 135.,
+}
 
 signal_model = 'double'
 #signal_model = 'triple'
@@ -21,214 +28,13 @@ cats = ['cat'+str(i).zfill(2) for i in xrange(16)]
 gROOT.SetBatch(kTRUE)
 RooMsgService.instance().setGlobalKillBelow(5)
 
-sig_processes = ['VBF', 'GluGlu', 'WMinusH', 'WPlusH', 'ZH']
-
-
-
-
-
-## ____________________________________________________________________________
-def build_mass_var(ws):
-    # create dimuon mass observable
-    ws.factory('x[125.0, {0}, {1}]'.format(range_low, range_high))
-    ws.var('x').setRange('signal_region', blind_low,blind_high)
-    ws.var('x').setRange('signal_fit', sig_fit_low,sig_fit_high)
-    ws.var('x').setRange('blinded_low', range_low,blind_low)
-    ws.var('x').setRange('blinded_high', blind_high,range_high)
-    ws.var('x').setRange('full_range', range_low,range_high)
-    ws.var('x').SetTitle('M_{#mu#mu}')
-    ws.var('x').setUnit('GeV')
-    # define the set obs=(x) and make it known to python
-    ws.defineSet('obs', 'x')
-
-
-## ____________________________________________________________________________
-def build_double_gaus(ws, cat_, proc):
-    cat = cat_ + '_' + proc
-    # RooRealVars
-    ws.factory('mean1_sig_model_'+cat+'[125., 121., 129.]')
-    ws.factory('sigma1_sig_model_'+cat+'[2., 1., 20.]')
-    ws.var('mean1_sig_model_'+cat).setUnit('GeV')
-    ws.var('sigma1_sig_model_'+cat).setUnit('GeV')
-    ws.factory('mean2_sig_model_'+cat+'[125., 121., 129.]')
-    ws.factory('sigma2_sig_model_'+cat+'[2., 1., 20.]')
-    ws.var('mean2_sig_model_'+cat).setUnit('GeV')
-    ws.var('sigma2_sig_model_'+cat).setUnit('GeV')
-    ws.factory('coef1_sig_model_'+cat+'[0.25, 0., 1.]')
-    # RooAbsPdfs
-    g1 = ws.factory(('Gaussian::g1_sig_model_{cat}(x, mean1_sig_model_{cat},'
-                     'sigma1_sig_model_{cat})').format(cat=cat))
-    g2 = ws.factory(('Gaussian::g2_sig_model_{cat}(x, mean2_sig_model_{cat},'
-                     'sigma2_sig_model_{cat})').format(cat=cat))
-    gaussians = RooArgList(g1, g2)
-    betas = RooArgList(ws.var('coef1_sig_model_'+cat))
-
-    sig_model = RooAddPdf('sig_model_'+cat, 'sig_model_'+cat,
-        gaussians, betas, kTRUE)
-    getattr(ws, 'import')(sig_model, RooFit.RecycleConflictNodes())
-    return sig_model
-
-
-
-## ____________________________________________________________________________
-def build_triple_gaus(ws, cat_, proc):
-    cat = cat_ + '_' + proc
-    # RooRealVars
-    ws.factory('mean1_sig_model_'+cat+'[125., 121., 129.]')
-    ws.factory('sigma1_sig_model_'+cat+'[2., 1., 20.]')
-    ws.var('mean1_sig_model_'+cat).setUnit('GeV')
-    ws.var('sigma1_sig_model_'+cat).setUnit('GeV')
-    ws.factory('mean2_sig_model_'+cat+'[125., 121., 129.]')
-    ws.factory('sigma2_sig_model_'+cat+'[2., 1., 20.]')
-    ws.var('mean2_sig_model_'+cat).setUnit('GeV')
-    ws.var('sigma2_sig_model_'+cat).setUnit('GeV')
-    ws.factory('mean3_sig_model_'+cat+'[125., 121., 129.]')
-    ws.factory('sigma3_sig_model_'+cat+'[2., 1., 20.]')
-    ws.var('mean3_sig_model_'+cat).setUnit('GeV')
-    ws.var('sigma3_sig_model_'+cat).setUnit('GeV')
-    ws.factory('coef1_sig_model_'+cat+'[0.5, 0., 1.]')
-    ws.factory('coef2_sig_model_'+cat+'[0.5, 0., 1.]')
-    # RooAbsPdfs
-    #ws.var('mean1_sig_model').setConstant()
-    #ws.var('mean2_sig_model').setConstant()
-    #ws.var('mean3_sig_model').setConstant()
-    g1 = ws.factory(('Gaussian::g1_sig_model_{cat}(x, mean1_sig_model_{cat},'
-                     'sigma1_sig_model_{cat})').format(cat=cat))
-    g2 = ws.factory(('Gaussian::g2_sig_model_{cat}(x, mean2_sig_model_{cat},'
-                     'sigma2_sig_model_{cat})').format(cat=cat))
-    g3 = ws.factory(('Gaussian::g3_sig_model_{cat}(x, mean3_sig_model_{cat},'
-                     'sigma3_sig_model_{cat})').format(cat=cat))
-    gaussians = RooArgList(g1, g2, g3)
-    betas = RooArgList(ws.var('coef1_sig_model_'+cat),
-        ws.var('coef2_sig_model_'+cat))
-
-    sig_model = RooAddPdf('sig_model_'+cat, 'sig_model_'+cat,
-        gaussians, betas, kTRUE)
-    getattr(ws, 'import')(sig_model, RooFit.RecycleConflictNodes())
-    return sig_model
-
-
-## ____________________________________________________________________________
-def build_sum_exp(ws, cat, order=4):
-    # RooRealVars
-    lambdas = RooArgList()
-    betas   = RooArgList()
-    for i in xrange(1, order+1):
-        ws.factory('alpha{0}_bkg_model_{cat}[{1}, -1., 0.]'.format(
-            i, max(-1., -0.04*(i+1)), cat=cat))
-        if i < order:
-            ws.factory('beta{0}_bkg_model_{cat}[{1}, 0.0001, .9999]'.format(
-                i, 0.9-float(i-1)*1./order, cat=cat))
-            betas.add(ws.var('beta{0}_bkg_model_{cat}'.format(i, cat=cat)))
-    # RooAbsPdfs
-    for i in xrange(1, order+1):
-        ws.factory(('Exponential::lambda{0}_bkg_model_{cat}(x,'
-            ' alpha{0}_bkg_model_{cat})').format(i, cat=cat))
-        lambdas.add(ws.pdf('lambda{0}_bkg_model_{cat}'.format(i, cat=cat)))
-    
-    bkg_model = RooAddPdf('bkg_model_'+cat, 'bkg_model_'+cat,
-        lambdas, betas, kTRUE)
-    getattr(ws, 'import')(bkg_model, RooFit.RecycleConflictNodes())
-    return bkg_model
-
-
-## ____________________________________________________________________________
-def build_data_dist(ws, cat, tfile):
-    hname = 'categories/hDiMuInvMass_'+cat
-    h = TH1F()
-    try:
-        h = tfile.Get(hname)
-    except:
-        print 'Error getting '+hname
-        return
-    # create binned dataset from histograms and add to workspace
-    dataset = RooDataHist('data_obs_'+cat, 'data_obs_'+cat, RooArgList(ws.var('x')), h)
-    # how to use the import method in PyROOT
-    getattr(ws,'import')(dataset, RooCmdArg(), RooFit.RecycleConflictNodes())
-    return dataset
-
-
-
-
-## ____________________________________________________________________________
-def get_mc_info(tfile):
-    try:
-        # get xsec and sumw/nevents
-        tsummary = tfile.Get('Summary')
-        tsummary.GetEntry(0)
-        xsec = tsummary.tCrossSec
-        sumw = 0.
-        nevt = 0
-        for entry in range(tsummary.GetEntries()):
-            tsummary.GetEntry(entry)
-            sumw += tsummary.tSumWts
-            nevt += tsummary.tNumEvts
-    except:
-        print 'Could not get summary info from {0}'.format(tfile)
-        xsec = -1.
-        sumw = -1.
-        nevt = -1
-    if nevt and not sumw: sumw = nevt
-    return xsec, sumw
-
-
-
-## ____________________________________________________________________________
-def build_mc_dist(tfile, ws, cat, proc):
-    hname = 'categories/hDiMuInvMass_'+cat
-    xsec, sumw = get_mc_info(tfile)
-    try:
-        h = tfile.Get(hname)
-    except:
-        print 'ERROR: could not get {0} from {1}.'.format(hname, tfile)
-        return False
-    h.Scale((lumi*xsec)/sumw)
-    # create binned dataset from histograms and add to workspace
-    mcset = RooDataHist('sig_hist_'+cat+'_'+proc, 'sig_hist_'+cat+'_'+proc,
-        RooArgList(ws.var('x')), h)
-    # how to use the import method in PyROOT
-    getattr(ws,'import')(mcset, RooCmdArg(), RooFit.RecycleConflictNodes())
-    return mcset
-
-
-## ____________________________________________________________________________
-def save_plot_of(ws, cat, proc, pdf, dist):
-    sig_color = kBlack
-    if proc=='VBF': sig_color = kGreen
-    elif proc=='GluGlu': sig_color = kBlue
-    elif proc=='WMinusH': sig_color = kRed
-    elif proc=='WPlusH': sig_color = kViolet
-    elif proc=='ZH': sig_color = kMagenta
-    canv = TCanvas(cat, cat, 1200, 900)
-    canv.cd()
-    thisframe = ws.var('x').frame()
-    dist.plotOn(thisframe, RooFit.DrawOption('hist'))
-    pdf.plotOn(thisframe, RooFit.LineColor(sig_color),
-        RooFit.Name(signal_model+'Gaus_'+cat+'_'+proc),
-        RooFit.Range('signal_fit'))
-    pdf.paramOn(thisframe, RooFit.Layout(0.55, 0.95, 0.74))
-    thisframe.getAttText().SetTextSize(0.02)
-    leg = TLegend(0.65, 0.75, 0.9, 0.9)
-    leg.AddEntry(thisframe.findObject(signal_model+'Gaus_'+cat+'_'+proc),
-        signal_model+'Gaus_'+cat+'_'+proc, 'l')
-    leg.AddEntry(thisframe.findObject('sig_hist_'+cat+'_'+proc),
-        'sig_hist_'+cat+'_'+proc, 'P')
-    thisframe.SetTitle(('{0} sig. only fit, M_{{#mu#mu}} ({1}, '
-        '{2}/pb)').format(proc, cat, lumi))
-    thisframe.Draw()
-    leg.Draw()
-    gPad.Modified()
-    canv.Print('sig_'+signal_model+'_fit_'+cat+'_'+proc+'.png')
-
-
-
 
 ## ____________________________________________________________________________
 def main():
     
     # create roo workspace
     w = RooWorkspace('mumu')
-    build_mass_var(w)
+    build_mass_var(w, ranges)
     obs = w.set('obs')
     # get data_obs for each bin
     fname_data = '{0}/{1}SingleMuon_Run2016{2}'.format(
@@ -253,46 +59,66 @@ def main():
     except:
         print 'Error opening input files'
         return
-    # get histograms
     for cat in cats:
         # get data RooDataHist
         data_obs = build_data_dist(w, cat, f0)
         # create background model
         bkg_model = build_sum_exp(w, cat)
+        # get signal histograms
+        sig_h_vbf = get_mc_hist(f1, cat, lumi, 'VBF')
+        sig_h_ggf = get_mc_hist(f2, cat, lumi, 'GluGlu')
+        sig_h_wm  = get_mc_hist(f3, cat, lumi, 'WMinusH')
+        sig_h_wp  = get_mc_hist(f4, cat, lumi, 'WPlusH')
+        sig_h_zh  = get_mc_hist(f5, cat, lumi, 'ZH')
+
+        iv_vbf = get_initial_vals_from_TH1(sig_h_vbf, signal_model)
+        iv_ggf = get_initial_vals_from_TH1(sig_h_ggf, signal_model)
+        iv_wm  = get_initial_vals_from_TH1(sig_h_wm, signal_model)
+        iv_wp  = get_initial_vals_from_TH1(sig_h_wp, signal_model)
+        iv_zh  = get_initial_vals_from_TH1(sig_h_zh, signal_model)
+
         # create signal models
         if signal_model=='triple':
-            sig_model_vbf = build_triple_gaus(w, cat, 'VBF')
-            sig_model_ggf = build_triple_gaus(w, cat, 'GluGlu')
-            sig_model_wm  = build_triple_gaus(w, cat, 'WMinusH')
-            sig_model_wp  = build_triple_gaus(w, cat, 'WPlusH')
-            sig_model_zh  = build_triple_gaus(w, cat, 'ZH')
+            sig_model_vbf = build_triple_gaus(w, cat, 'VBF', iv_vbf)
+            sig_model_ggf = build_triple_gaus(w, cat, 'GluGlu', iv_ggf)
+            sig_model_wm  = build_triple_gaus(w, cat, 'WMinusH', iv_wm)
+            sig_model_wp  = build_triple_gaus(w, cat, 'WPlusH', iv_wp)
+            sig_model_zh  = build_triple_gaus(w, cat, 'ZH', iv_zh)
         elif signal_model=='double':
-            sig_model_vbf = build_double_gaus(w, cat, 'VBF')
-            sig_model_ggf = build_double_gaus(w, cat, 'GluGlu')
-            sig_model_wm  = build_double_gaus(w, cat, 'WMinusH')
-            sig_model_wp  = build_double_gaus(w, cat, 'WPlusH')
-            sig_model_zh  = build_double_gaus(w, cat, 'ZH')
+            sig_model_vbf = build_double_gaus(w, cat, 'VBF', iv_vbf)
+            sig_model_ggf = build_double_gaus(w, cat, 'GluGlu', iv_ggf)
+            sig_model_wm  = build_double_gaus(w, cat, 'WMinusH', iv_wm)
+            sig_model_wp  = build_double_gaus(w, cat, 'WPlusH', iv_wp)
+            sig_model_zh  = build_double_gaus(w, cat, 'ZH', iv_zh)
+
         # get signal MC RooDataHists
-        sig_dist_vbf = build_mc_dist(f1, w, cat, 'VBF')
-        sig_dist_ggf = build_mc_dist(f2, w, cat, 'GluGlu')
-        sig_dist_wm  = build_mc_dist(f3, w, cat, 'WMinusH')
-        sig_dist_wp  = build_mc_dist(f4, w, cat, 'WPlusH')
-        sig_dist_zh  = build_mc_dist(f5, w, cat, 'ZH')
+        sig_dist_vbf = build_mc_dist(sig_h_vbf, w)
+        sig_dist_ggf = build_mc_dist(sig_h_ggf, w)
+        sig_dist_wm  = build_mc_dist(sig_h_wm, w)
+        sig_dist_wp  = build_mc_dist(sig_h_wp, w)
+        sig_dist_zh  = build_mc_dist(sig_h_zh, w)
+
+
         # fit signal models to signal MC dists
         fit_result_vbf = sig_model_vbf.fitTo(sig_dist_vbf,
             RooFit.Save(), RooFit.Range('signal_fit'),
+            RooFit.NormRange('signal_fit'),
             RooFit.SumW2Error(kTRUE))
         fit_result_ggf = sig_model_ggf.fitTo(sig_dist_ggf,
             RooFit.Save(), RooFit.Range('signal_fit'),
+            RooFit.NormRange('signal_fit'),
             RooFit.SumW2Error(kTRUE))
         fit_result_wm = sig_model_wm.fitTo(sig_dist_wm,
             RooFit.Save(), RooFit.Range('signal_fit'),
+            RooFit.NormRange('signal_fit'),
             RooFit.SumW2Error(kTRUE))
         fit_result_wp = sig_model_wp.fitTo(sig_dist_wp,
             RooFit.Save(), RooFit.Range('signal_fit'),
+            RooFit.NormRange('signal_fit'),
             RooFit.SumW2Error(kTRUE))
         fit_result_zh = sig_model_zh.fitTo(sig_dist_zh,
             RooFit.Save(), RooFit.Range('signal_fit'),
+            RooFit.NormRange('signal_fit'),
             RooFit.SumW2Error(kTRUE))
         # fix parameters of signal fits
 
@@ -300,11 +126,11 @@ def main():
 
 
         # plot and print results for later viewing pleasure
-        save_plot_of(w, cat, 'VBF', sig_model_vbf, sig_dist_vbf)
-        save_plot_of(w, cat, 'GluGlu', sig_model_ggf, sig_dist_ggf)
-        save_plot_of(w, cat, 'WMinusH', sig_model_wm, sig_dist_wm)
-        save_plot_of(w, cat, 'WPlusH', sig_model_wp, sig_dist_wp)
-        save_plot_of(w, cat, 'ZH', sig_model_zh, sig_dist_zh)
+        save_plot_of(w, cat, lumi, signal_model, 'VBF', sig_model_vbf, sig_dist_vbf)
+        save_plot_of(w, cat, lumi, signal_model, 'GluGlu', sig_model_ggf, sig_dist_ggf)
+        save_plot_of(w, cat, lumi, signal_model, 'WMinusH', sig_model_wm, sig_dist_wm)
+        save_plot_of(w, cat, lumi, signal_model, 'WPlusH', sig_model_wp, sig_dist_wp)
+        save_plot_of(w, cat, lumi, signal_model, 'ZH', sig_model_zh, sig_dist_zh)
 
 
 
@@ -313,7 +139,7 @@ def main():
     f_out = TFile('{0}/{1}'.format(shape_data_dir, output_file), 'RECREATE')
     f_out.cd()
     # save workspace in output file
-    w.Print('v')
+#    w.Print('v')
     w.Write()
     f_out.Write()
     f_out.Close()
---
+++ Shape/python/tools/tools.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+import os, sys
+from ROOT import *
+
+## ____________________________________________________________________________
+def build_mass_var(ws, r):
+    # create dimuon mass observable
+    ws.factory('x[125.0, {0}, {1}]'.format(r['range_low'], r['range_high']))
+    ws.var('x').setRange('signal_region', r['blind_low'],r['blind_high'])
+    ws.var('x').setRange('signal_fit', r['sig_fit_low'],r['sig_fit_high'])
+    ws.var('x').setRange('blinded_low', r['range_low'],r['blind_low'])
+    ws.var('x').setRange('blinded_high', r['blind_high'],r['range_high'])
+    ws.var('x').setRange('full_range', r['range_low'],r['range_high'])
+    ws.var('x').SetTitle('M_{#mu#mu}')
+    ws.var('x').setUnit('GeV')
+    # define the set obs=(x) and make it known to python
+    ws.defineSet('obs', 'x')
+
+## ____________________________________________________________________________
+def get_initial_vals_from_TH1(th1, signal_model):
+    sigma_max = 10.
+    mean_min = 123.
+    mean_max = 127.
+    initial_values = {}
+    if signal_model=='single':
+        initial_values = {
+            'mean1' : th1.GetMean(),
+            'mean1min' : max(mean_min, th1.GetMean() - 2*th1.GetRMS()),
+            'mean1max' : min(mean_max, th1.GetMean() + 2*th1.GetRMS()),
+            'sigma1' : th1.GetRMS(),
+            'sigma1min' : 0,
+            'sigma1max' : min(sigma_max, 2*th1.GetRMS()),
+            'mean2' : th1.GetMean(),
+            'mean2min' : max(mean_min, th1.GetMean() - 2*th1.GetRMS()),
+            'mean2max' : min(mean_max, th1.GetMean() + 2*th1.GetRMS()),
+            'sigma2' : th1.GetRMS(),
+            'sigma2min' : 0,
+            'sigma2max' : min(sigma_max, 2*th1.GetRMS()),
+            'coef1' : 0.28, 'coef1min' : 0, 'coef1max' : 1
+        }
+    elif signal_model=='double':
+        initial_values = {
+            'mean1' : th1.GetMean(),
+            'mean1min' : max(mean_min, th1.GetMean() - 2*th1.GetRMS()),
+            'mean1max' : min(mean_max, th1.GetMean() + 2*th1.GetRMS()),
+            'sigma1' : th1.GetRMS(),
+            'sigma1min' : 0,
+            'sigma1max' : min(sigma_max, 2*th1.GetRMS()),
+            'mean2' : th1.GetMean(),
+            'mean2min' : max(mean_min, th1.GetMean() - 2*th1.GetRMS()),
+            'mean2max' : min(mean_max, th1.GetMean() + 2*th1.GetRMS()),
+            'sigma2' : th1.GetRMS(),
+            'sigma2min' : 0,
+            'sigma2max' : min(sigma_max, 2*th1.GetRMS()),
+            'coef1' : 0.28, 'coef1min' : 0, 'coef1max' : 1
+        }
+    #elif signal_model=='triple':
+    #    initial_values = {
+    #        'mean1' : th1.GetMean(),
+    #        'mean1min' : max(120., th1.GetMean() - 0.2*th1.GetRMS()),
+    #        'mean1max' : min(130., th1.GetMean() + 0.2*th1.GetRMS()),
+    #        'sigma1' : 0.6*th1.GetRMS(),
+    #        'sigma1min' : 0.2*th1.GetRMS(),
+    #        'sigma1max' : 1.2*th1.GetRMS(),
+    #        'mean2' : th1.GetMean(), 
+    #        'mean2min' : max(120., th1.GetMean() - 0.4*th1.GetRMS()),
+    #        'mean2max' : min(130., th1.GetMean() + 0.4*th1.GetRMS()),
+    #        'sigma2' : 1.0*th1.GetRMS(), 
+    #        'sigma2min' : 0.4*th1.GetRMS(),
+    #        'sigma2max' : 1.8*th1.GetRMS(),
+    #        'mean3' : th1.GetMean(),
+    #        'mean3min' : max(120., th1.GetMean() - 2.4*th1.GetRMS()),
+    #        'mean3max' : min(130., th1.GetMean() + 0.8*th1.GetRMS()),
+    #        'sigma3' : 2.4*th1.GetRMS(),
+    #        'sigma3min' : 1.2*th1.GetRMS(),
+    #        'sigma3max' : 4.8*th1.GetRMS(),
+    #        'coef1' : 0.7, 'coef1min' : 0.5, 'coef1max' : 1,
+    #        'coef2' : 0.6, 'coef2min' : 0.0, 'coef2max' : 1
+    #    }
+
+    for key, val in initial_values.iteritems():
+        print key, ':', val
+
+    return initial_values
+
+## ____________________________________________________________________________
+def build_double_gaus(ws, cat_, proc, iv):
+    cat = cat_ + '_' + proc
+    # RooRealVars
+    ws.factory(('mean1_sig_model_'+cat
+        +'[{mean1}, {mean1min}, {mean1max}]').format(**iv))
+    ws.factory(('sigma1_sig_model_'+cat
+        +'[{sigma1}, {sigma1min}, {sigma1max}]').format(**iv))
+    ws.factory(('mean2_sig_model_'+cat
+        +'[{mean2}, {mean2min}, {mean2max}]').format(**iv))
+    ws.factory(('sigma2_sig_model_'+cat
+        +'[{sigma2}, {sigma2min}, {sigma2max}]').format(**iv))
+    ws.factory(('coef1_sig_model_'+cat
+        +'[{coef1}, {coef1min}, {coef1max}]').format(**iv))
+
+    # RooAbsPdfs
+    g1 = ws.factory(('Gaussian::g1_sig_model_{cat}(x, mean1_sig_model_{cat},'
+                     'sigma1_sig_model_{cat})').format(cat=cat))
+    g2 = ws.factory(('Gaussian::g2_sig_model_{cat}(x, mean2_sig_model_{cat},'
+                     'sigma2_sig_model_{cat})').format(cat=cat))
+    gaussians = RooArgList(g1, g2)
+    betas = RooArgList(ws.var('coef1_sig_model_'+cat))
+
+    ws.var('mean1_sig_model_'+cat).setUnit('GeV')
+    ws.var('sigma1_sig_model_'+cat).setUnit('GeV')
+    ws.var('mean2_sig_model_'+cat).setUnit('GeV')
+    ws.var('sigma2_sig_model_'+cat).setUnit('GeV')
+
+    sig_model = RooAddPdf('sig_model_'+cat, 'sig_model_'+cat,
+        gaussians, betas, kTRUE)
+    getattr(ws, 'import')(sig_model, RooFit.RecycleConflictNodes())
+    return sig_model
+
+## ____________________________________________________________________________
+def build_triple_gaus(ws, cat_, proc):
+    cat = cat_ + '_' + proc
+    # RooRealVars
+    ws.factory('mean1_sig_model_'+cat+'[125., 121., 129.]')
+    ws.factory('sigma1_sig_model_'+cat+'[2., 1., 20.]')
+    ws.var('mean1_sig_model_'+cat).setUnit('GeV')
+    ws.var('sigma1_sig_model_'+cat).setUnit('GeV')
+    ws.factory('mean2_sig_model_'+cat+'[125., 121., 129.]')
+    ws.factory('sigma2_sig_model_'+cat+'[2., 1., 20.]')
+    ws.var('mean2_sig_model_'+cat).setUnit('GeV')
+    ws.var('sigma2_sig_model_'+cat).setUnit('GeV')
+    ws.factory('mean3_sig_model_'+cat+'[125., 121., 129.]')
+    ws.factory('sigma3_sig_model_'+cat+'[2., 1., 20.]')
+    ws.var('mean3_sig_model_'+cat).setUnit('GeV')
+    ws.var('sigma3_sig_model_'+cat).setUnit('GeV')
+    ws.factory('coef1_sig_model_'+cat+'[0.5, 0., 1.]')
+    ws.factory('coef2_sig_model_'+cat+'[0.5, 0., 1.]')
+    # RooAbsPdfs
+    g1 = ws.factory(('Gaussian::g1_sig_model_{cat}(x, mean1_sig_model_{cat},'
+                     'sigma1_sig_model_{cat})').format(cat=cat))
+    g2 = ws.factory(('Gaussian::g2_sig_model_{cat}(x, mean2_sig_model_{cat},'
+                     'sigma2_sig_model_{cat})').format(cat=cat))
+    g3 = ws.factory(('Gaussian::g3_sig_model_{cat}(x, mean3_sig_model_{cat},'
+                     'sigma3_sig_model_{cat})').format(cat=cat))
+    gaussians = RooArgList(g1, g2, g3)
+    betas = RooArgList(ws.var('coef1_sig_model_'+cat),
+        ws.var('coef2_sig_model_'+cat))
+
+    sig_model = RooAddPdf('sig_model_'+cat, 'sig_model_'+cat,
+        gaussians, betas, kTRUE)
+    getattr(ws, 'import')(sig_model, RooFit.RecycleConflictNodes())
+    return sig_model
+
+
+## ____________________________________________________________________________
+def build_sum_exp(ws, cat, order=4):
+    # RooRealVars
+    lambdas = RooArgList()
+    betas   = RooArgList()
+    for i in xrange(1, order+1):
+        ws.factory('alpha{0}_bkg_model_{cat}[{1}, -1., 0.]'.format(
+            i, max(-1., -0.04*(i+1)), cat=cat))
+        if i < order:
+            ws.factory('beta{0}_bkg_model_{cat}[{1}, 0.0001, .9999]'.format(
+                i, 0.9-float(i-1)*1./order, cat=cat))
+            betas.add(ws.var('beta{0}_bkg_model_{cat}'.format(i, cat=cat)))
+    # RooAbsPdfs
+    for i in xrange(1, order+1):
+        ws.factory(('Exponential::lambda{0}_bkg_model_{cat}(x,'
+            ' alpha{0}_bkg_model_{cat})').format(i, cat=cat))
+        lambdas.add(ws.pdf('lambda{0}_bkg_model_{cat}'.format(i, cat=cat)))
+    
+    bkg_model = RooAddPdf('bkg_model_'+cat, 'bkg_model_'+cat,
+        lambdas, betas, kTRUE)
+    getattr(ws, 'import')(bkg_model, RooFit.RecycleConflictNodes())
+    return bkg_model
+
+
+## ____________________________________________________________________________
+def build_data_dist(ws, cat, tfile):
+    hname = 'categories/hDiMuInvMass_'+cat
+    h = TH1F()
+    try:
+        h = tfile.Get(hname)
+    except:
+        print 'Error getting '+hname
+        return
+    # create binned dataset from histograms and add to workspace
+    dataset = RooDataHist('data_obs_'+cat, 'data_obs_'+cat, RooArgList(ws.var('x')), h)
+    # how to use the import method in PyROOT
+    getattr(ws,'import')(dataset, RooCmdArg(), RooFit.RecycleConflictNodes())
+    return dataset
+
+## ____________________________________________________________________________
+def get_mc_info(tfile):
+    try:
+        # get xsec and sumw/nevents
+        tsummary = tfile.Get('Summary')
+        tsummary.GetEntry(0)
+        xsec = tsummary.tCrossSec
+        sumw = 0.
+        nevt = 0
+        for entry in range(tsummary.GetEntries()):
+            tsummary.GetEntry(entry)
+            sumw += tsummary.tSumWts
+            nevt += tsummary.tNumEvts
+    except:
+        print 'Could not get summary info from {0}'.format(tfile)
+        xsec = -1.
+        sumw = -1.
+        nevt = -1
+    if nevt and not sumw: sumw = nevt
+    return xsec, sumw
+
+## ____________________________________________________________________________
+def get_mc_hist(tfile, cat, lumi, proc):
+    hname = 'categories/hDiMuInvMass_'+cat
+    xsec, sumw = get_mc_info(tfile)
+    try:
+        h = tfile.Get(hname)
+    except:
+        print 'ERROR: could not get {0} from {1}.'.format(hname, tfile)
+        return False
+    h.Scale((lumi*xsec)/sumw)
+    h.SetName('sig_hist_'+cat+'_'+proc)
+    return h
+
+
+## ____________________________________________________________________________
+def build_mc_dist(h, ws):
+    hname = h.GetName()
+    # create binned dataset from histograms and add to workspace
+    mcset = RooDataHist(hname, hname, RooArgList(ws.var('x')), h)
+    # how to use the import method in PyROOT
+    getattr(ws,'import')(mcset, RooCmdArg(), RooFit.RecycleConflictNodes())
+    return mcset
+
+
+## ____________________________________________________________________________
+def save_plot_of(ws, cat, lumi, signal_model, proc, pdf, dist):
+    sig_color = kBlack
+    if proc=='VBF': sig_color = kGreen
+    elif proc=='GluGlu': sig_color = kBlue
+    elif proc=='WMinusH': sig_color = kRed
+    elif proc=='WPlusH': sig_color = kViolet
+    elif proc=='ZH': sig_color = kMagenta
+    canv = TCanvas(cat, cat, 1200, 900)
+    canv.cd()
+    thisframe = ws.var('x').frame()
+    dist.plotOn(thisframe, RooFit.DrawOption('hist'))
+    pdf.plotOn(thisframe, RooFit.LineColor(sig_color),
+        RooFit.Name(signal_model+'Gaus_'+cat+'_'+proc),
+        RooFit.Range('signal_fit'))
+    pdf.paramOn(thisframe, RooFit.Layout(0.55, 0.95, 0.74))
+    thisframe.getAttText().SetTextSize(0.02)
+    leg = TLegend(0.65, 0.75, 0.9, 0.9)
+    leg.AddEntry(thisframe.findObject(signal_model+'Gaus_'+cat+'_'+proc),
+        signal_model+'Gaus_'+cat+'_'+proc, 'l')
+    leg.AddEntry(thisframe.findObject('sig_hist_'+cat+'_'+proc),
+        'sig_hist_'+cat+'_'+proc, 'P')
+    thisframe.SetTitle(('{0} sig. only fit, M_{{#mu#mu}} ({1}, '
+        '{2}/pb)').format(proc, cat, lumi))
+    thisframe.Draw()
+    leg.Draw()
+    gPad.Modified()
+    canv.Print('sig_'+signal_model+'_fit_'+cat+'_'+proc+'.png')
+
+
