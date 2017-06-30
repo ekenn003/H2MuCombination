148f478a9d57661522540b1a8fe95b01a679284e
E Kennedy <ekennedy@cern.ch>
2017-06-30 05:41:06 +0200

    simplify per proc

--- Shape/python/prepareShapesFile.py
+++ Shape/python/prepareShapesFile.py
@@ -40,33 +40,33 @@ def main():
     w = RooWorkspace('mumu')
     build_mass_var(w, ranges)
     obs = w.set('obs')
-    # get data_obs for each bin
-    fname_data = '{0}/{1}SingleMuon_Run2016{2}'.format(
-        main_data_dir, input_file_head, input_file_tail)
-    fname_vbf  = '{0}/{1}VBF_HToMuMu{2}'.format(
-        main_data_dir, input_file_head, input_file_tail)
-    fname_ggf  = '{0}/{1}GluGlu_HToMuMu{2}'.format(
-        main_data_dir, input_file_head, input_file_tail)
-    fname_wm   = '{0}/{1}WMinusH_HToMuMu{2}'.format(
-        main_data_dir, input_file_head, input_file_tail)
-    fname_wp   = '{0}/{1}WPlusH_HToMuMu{2}'.format(
-        main_data_dir, input_file_head, input_file_tail)
-    fname_zh   = '{0}/{1}ZH_HToMuMu{2}'.format(
-        main_data_dir, input_file_head, input_file_tail)
+    # map of filenames
+    fnames = {
+        'data' : '{0}/{1}{2}{3}'.format(main_data_dir,
+                     input_file_head, 'SingleMuon_Run2016', input_file_tail)
+    }
+    for p in sp:
+        fnames[p] = '{0}/{1}{2}_HToMuMu{3}'.format(main_data_dir,
+                    input_file_head, p, input_file_tail)
+    # map of tfiles
+    tf = {}
     try:
-        f0 = TFile.Open(fname_data)
-        f1 = TFile.Open(fname_vbf)
-        f2 = TFile.Open(fname_ggf)
-        f3 = TFile.Open(fname_wm)
-        f4 = TFile.Open(fname_wp)
-        f5 = TFile.Open(fname_zh)
+        tf['data'] = TFile.Open(fnames['data'])
     except:
-        print 'Error opening input files'
+        print 'Error opening input file ' + fnames['data']
         return
+    for p in sp:
+        try:
+            tf[p] = TFile.Open(fnames[p])
+        except:
+            print 'Error opening input file ' + fnames[p]
+            return
+
+
     for cat in cats:
         #if cat != 'cat00': continue
         # get data RooDataHist
-        data_obs = build_data_dist(w, cat, f0)
+        data_obs = build_data_dist(w, cat, tf['data'])
         # create background model
         bkg_model = build_sum_exp(w, cat)
         # create bkg norm
@@ -74,83 +74,43 @@ def main():
         bkg_model_norm = RooRealVar('bkg_model_'+cat+'_norm',
             'bkg_model_'+cat+'_norm', data_sume/2., data_sume*2., 'Events')
         getattr(w, 'import')(bkg_model_norm, RooFit.RecycleConflictNodes())
-        # get signal histograms
-        sig_h_vbf = get_mc_hist(f1, cat, lumi, 'VBF')
-        sig_h_ggf = get_mc_hist(f2, cat, lumi, 'GluGlu')
-        sig_h_wm  = get_mc_hist(f3, cat, lumi, 'WMinusH')
-        sig_h_wp  = get_mc_hist(f4, cat, lumi, 'WPlusH')
-        sig_h_zh  = get_mc_hist(f5, cat, lumi, 'ZH')
 
-        iv_vbf = get_initial_vals_from_TH1(sig_h_vbf, cat, 'VBF')
-        iv_ggf = get_initial_vals_from_TH1(sig_h_ggf, cat, 'GluGlu')
-        iv_wm  = get_initial_vals_from_TH1(sig_h_wm, cat, 'WMinusH')
-        iv_wp  = get_initial_vals_from_TH1(sig_h_wp, cat, 'WPlusH')
-        iv_zh  = get_initial_vals_from_TH1(sig_h_zh, cat, 'ZH')
+        s_hists = {}
+        s_dists = {}
+        s_ivs = {}
+        s_models = {}
+        s_fits = {}
+        for p in sp:
+            # get signal histograms
+            s_hists[p] = get_mc_hist(tf[p], cat, lumi, p)
 
-        # create signal models
-        if signal_model=='triple':
-            sig_model_vbf = build_triple_gaus(w, cat, 'VBF', iv_vbf)
-            sig_model_ggf = build_triple_gaus(w, cat, 'GluGlu', iv_ggf)
-            sig_model_wm  = build_triple_gaus(w, cat, 'WMinusH', iv_wm)
-            sig_model_wp  = build_triple_gaus(w, cat, 'WPlusH', iv_wp)
-            sig_model_zh  = build_triple_gaus(w, cat, 'ZH', iv_zh)
-        elif signal_model=='double':
-            sig_model_vbf = build_double_gaus(w, cat, 'VBF', iv_vbf)
-            sig_model_ggf = build_double_gaus(w, cat, 'GluGlu', iv_ggf)
-            sig_model_wm  = build_double_gaus(w, cat, 'WMinusH', iv_wm)
-            sig_model_wp  = build_double_gaus(w, cat, 'WPlusH', iv_wp)
-            sig_model_zh  = build_double_gaus(w, cat, 'ZH', iv_zh)
-        elif signal_model=='single':
-            sig_model_vbf = build_single_gaus(w, cat, 'VBF', iv_vbf)
-            sig_model_ggf = build_single_gaus(w, cat, 'GluGlu', iv_ggf)
-            sig_model_wm  = build_single_gaus(w, cat, 'WMinusH', iv_wm)
-            sig_model_wp  = build_single_gaus(w, cat, 'WPlusH', iv_wp)
-            sig_model_zh  = build_single_gaus(w, cat, 'ZH', iv_zh)
+            # get initial guesses for fit from histogram
+            s_ivs[p] = get_initial_vals_from_TH1(s_hists[p], cat, p)
 
-        # get signal MC RooDataHists
-        sig_dist_vbf = build_mc_dist(sig_h_vbf, w)
-        sig_dist_ggf = build_mc_dist(sig_h_ggf, w)
-        sig_dist_wm  = build_mc_dist(sig_h_wm, w)
-        sig_dist_wp  = build_mc_dist(sig_h_wp, w)
-        sig_dist_zh  = build_mc_dist(sig_h_zh, w)
+            # create signal models
+            if signal_model=='triple':
+                s_models[p] = build_triple_gaus(w, cat, p, s_ivs[p])
+            elif signal_model=='double':
+                s_models[p] = build_double_gaus(w, cat, p, s_ivs[p])
+            elif signal_model=='single':
+                s_models[p] = build_single_gaus(w, cat, p, s_ivs[p])
+
+            # get signal MC RooDataHists
+            s_dists[p] = build_mc_dist(s_hists[p], w)
+
+            # signal fit results
+            s_fits[p] = s_models[p].fitTo(s_dists[p],
+                RooFit.Save(), RooFit.Range('signal_fit'),
+                RooFit.NormRange('signal_fit'),
+                RooFit.SumW2Error(kTRUE))
+
+            # fix parameters of signal fits
+            s_models[p].getParameters(RooArgSet(w.var('x'))
+                ).setAttribAll('Constant', kTRUE)
 
 
-        # fit signal models to signal MC dists
-        fit_result_vbf = sig_model_vbf.fitTo(sig_dist_vbf,
-            RooFit.Save(), RooFit.Range('signal_fit'),
-            RooFit.NormRange('signal_fit'),
-            RooFit.SumW2Error(kTRUE))
-        fit_result_ggf = sig_model_ggf.fitTo(sig_dist_ggf,
-            RooFit.Save(), RooFit.Range('signal_fit'),
-            RooFit.NormRange('signal_fit'),
-            RooFit.SumW2Error(kTRUE))
-        fit_result_wm = sig_model_wm.fitTo(sig_dist_wm,
-            RooFit.Save(), RooFit.Range('signal_fit'),
-            RooFit.NormRange('signal_fit'),
-            RooFit.SumW2Error(kTRUE))
-        fit_result_wp = sig_model_wp.fitTo(sig_dist_wp,
-            RooFit.Save(), RooFit.Range('signal_fit'),
-            RooFit.NormRange('signal_fit'),
-            RooFit.SumW2Error(kTRUE))
-        fit_result_zh = sig_model_zh.fitTo(sig_dist_zh,
-            RooFit.Save(), RooFit.Range('signal_fit'),
-            RooFit.NormRange('signal_fit'),
-            RooFit.SumW2Error(kTRUE))
-        # fix parameters of signal fits
-        sig_model_vbf.getParameters(RooArgSet(w.var('x'))).setAttribAll('Constant', kTRUE)
-        sig_model_ggf.getParameters(RooArgSet(w.var('x'))).setAttribAll('Constant', kTRUE)
-        sig_model_wm.getParameters(RooArgSet(w.var('x'))).setAttribAll('Constant', kTRUE)
-        sig_model_wp.getParameters(RooArgSet(w.var('x'))).setAttribAll('Constant', kTRUE)
-        sig_model_zh.getParameters(RooArgSet(w.var('x'))).setAttribAll('Constant', kTRUE)
-
-
-        # plot and print results for later viewing pleasure
-        save_plot_of(w, cat, lumi, signal_model, 'VBF', sig_model_vbf, sig_dist_vbf)
-        save_plot_of(w, cat, lumi, signal_model, 'GluGlu', sig_model_ggf, sig_dist_ggf)
-        save_plot_of(w, cat, lumi, signal_model, 'WMinusH', sig_model_wm, sig_dist_wm)
-        save_plot_of(w, cat, lumi, signal_model, 'WPlusH', sig_model_wp, sig_dist_wp)
-        save_plot_of(w, cat, lumi, signal_model, 'ZH', sig_model_zh, sig_dist_zh)
-
+            # plot and print results for later viewing pleasure
+            save_plot_of(w, cat, lumi, signal_model, p, s_models[p], s_dists[p])
 
 
     # create output file
@@ -164,12 +124,10 @@ def main():
     f_out.Close()
     print 'Created ' + '{0}/{1}'.format(shape_data_dir, output_file)
 
-    f0.Close()
-    f1.Close()
-    f2.Close()
-    f3.Close()
-    f4.Close()
-    f5.Close()
+    # close inputs
+    for p, f in tf.iteritems():
+        tf[p].Close()
+
 
 
 
